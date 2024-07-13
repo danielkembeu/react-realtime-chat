3eeff33d8f1bcfef007bd9133cbdc0cfbb2a3f04
danielkembeu <kembeudaniel2@gmail.com>
2024-07-13 17:45:52 +0100

    Realtime Chat app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+      Welcome to the app
+    </div>
+  )
+}
+
+export default App
